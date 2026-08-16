25851e0a80dd82780f97cb1ee478e0acb4052650
erzei <retro.erzei@gmail.com>
2026-08-15 20:18:45 -0600

    Initial project commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "main.hpp"
+
+int main() {
+    int screenWidth = 600;
+    int screenHeight = 1200;
+
+    raylib::Window window(screenWidth, screenHeight, "ray-base++");
+    SetTargetFPS(60);
+
+    while (!raylib::Window::ShouldClose()) {
+        window.BeginDrawing();
+        window.ClearBackground(raylib::Color::White());
+        raylib::DrawText("Hello, raylib-cpp!", 190, 200, 20, raylib::Color::DarkGray());
+        window.EndDrawing();
+    }
+
+    return 0;
+}
